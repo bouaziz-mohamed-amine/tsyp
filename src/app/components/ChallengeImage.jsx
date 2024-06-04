2751bb558c435b941837a74c823cc6da01c8bb61
unknown <https://github.com/ramezhadhri>
2024-06-04 13:08:44 +0100

    fixer la distance entre le titre 'tinia 2056 challenge '

--- src/app/components/ChallengeImage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-
-export default function ChallengeImage({ src, alt, loading = "lazy", ...props }) {
-	return <img {...props} src={src} alt={alt} loading={loading}  className="rounded-2xl"/>;
-}
