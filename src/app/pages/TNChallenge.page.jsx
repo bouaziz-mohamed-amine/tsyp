24c14c59a65ff3abf137ec0fae2adcd61c3fc450
amine <amine.m.a1998@gmail.com>
2024-06-06 22:34:35 +0100

    fix pdf title

--- src/app/pages/TNChallenge.page.jsx
+++ src/app/pages/TNChallenge.page.jsx
@@ -41,7 +41,7 @@ export function TNChallengePage() {
 						className="relative inline min-w-[120px] items-center justify-center rounded-full border-2 border-gray-600 px-6 py-2 text-center text-sm font-medium transition hover:border-gray-900 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-gray-400 dark:bg-black  dark:hover:bg-white dark:hover:bg-opacity-20 dark:focus:ring-blue-800"
 					>
 						{/* <div className="flex flex-wrap items-center justify-start gap-2"> */}
-						<span className="">Learn more TN 2056</span>
+						<span className="">Learn more about TN 2056 Challenge</span>
 						{/* </div> */}
 					</Link>
 				
