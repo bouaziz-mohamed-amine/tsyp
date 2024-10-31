00c360b4f07bd13b8e660490afb33ac7e8acb273
amine <amine.m.a1998@gmail.com>
2024-10-31 22:09:29 +0100

    fix tn challenge page

--- src/app/pages/CallForeignRegistrations.jsx
+++ src/app/pages/CallForeignRegistrations.jsx
@@ -6,14 +6,14 @@ import CustomImage from "../components/CustomImage";
 export default function ForeignRegistrationPage() {
 	return (
 		<div className="h-full">
-			<h1 className="mb-4 text-4xl font-bold lg:mt-8 text-center lg:text-5xl lg:font-extrabold lg:leading-none">
+			<h1 className="mb-4 text-4xl font-bold lg:mt-8 dark:text-gray-300 text-center lg:text-5xl lg:font-extrabold lg:leading-none">
 				IEEE Tunisian SYP Congress 2024
 			</h1>
-			<h3 className="mb-4 text-2xl font-bold lg:mb-8 text-center lg:text-2xl lg:font-extrabold lg:leading-none">
+			<h3 className="mb-4 text-2xl dark:text-gray-300 font-bold lg:mb-8 text-center lg:text-2xl lg:font-extrabold lg:leading-none">
 				Registration for the IEEE Tunisian SYP Congress 2024 is NOW OPEN for
 				FOREIGN IEEE Members!
 			</h3>
-			<h1 className=" text-lg font-medium  lg:text-start  lg:font-normal">
+			<h1 className=" text-lg font-medium  lg:text-start dark:text-gray-300 lg:font-normal">
 					IEEE Tunisian SYP Congress 2024 is co-organized by IEEE Tunisia Section & IEEE ENETCOM SB. It is scheduled to take place from the
 					<span className="font-bold"> 22nd to the 24th of December 2024, at El Medina Congress Center, Yasmine Hammamet, Tunisia.</span>
 			</h1>
@@ -26,32 +26,32 @@ export default function ForeignRegistrationPage() {
 					/>
 				</div>
 				<div className="col-span-6">
-					<h1 className=" text-base font-medium  lg:mb-3 lg:text-start  lg:font-normal">
+					<h1 className=" text-base font-medium dark:text-gray-300 lg:mb-3 lg:text-start  lg:font-normal">
 					Building on the success of previous events like Tunisian SYP11 2023 , R8SYP 2022, TSYPX 2022, NASYP 2021 all held in Tunisia, the expectations within the IEEE community have never been higher. This year, <span className="font-bold">Tunisian SYP12</span> continues its legacy as one of the biggest Tech congresses in Tunisia, with an audience of 1200+ national & international engineering students and young professionals. 
 					</h1>
-					<p className="mb-3 text-base font-bold text-gray-700 dark:text-gray-400 ">
+					<p className="mb-3 text-base font-bold text-gray-700 dark:text-gray-300 ">
 						IEEE Tunisian SYP is pleased to offer attendees a two packs option:
 					</p>
 
 					<ul class=" ml-4 list-disc">
-						<li className="ml-4 text-base font-normal text-gray-700 dark:text-gray-400 ">
+						<li className="ml-4 text-base font-normal text-gray-700 dark:text-gray-300 ">
 						<span className="font-bold">Event Registration (US$105 - Professional / US$85 - Student)</span>: Includes access to all technical and non-technical sessions, panel discussions, keynote speeches, ceremonies, TN2056 forum, Tunisian SYP EXPO, networking breaks, dinner on Days 1 and 2, lunch on Day 2, and social activities. A welcome kit is also provided.
 						</li>
-						<li className="ml-4 text-base font-normal text-gray-700 dark:text-gray-400 ">
+						<li className="ml-4 text-base font-normal text-gray-700 dark:text-gray-300 ">
 						<span className="font-bold">Full Registration Options (Professionals & Students)</span>:
 						Includes Event Registration and 2-night hotel accommodation (December 22nd - 24th):
 						<ul class=" ml-4 list-disc">
-						<li className="ml-4 text-base font-normal text-gray-700 dark:text-gray-400 ">
+						<li className="ml-4 text-base font-normal text-gray-700 dark:text-gray-300 ">
 						<span className="font-bold">Double Room:</span> US$205 - Professional / US$185 - Student ( US$50 per night)
 						</li>
-						<li className="ml-4 text-base font-normal text-gray-700 dark:text-gray-400 ">
+						<li className="ml-4 text-base font-normal text-gray-700 dark:text-gray-300 ">
 						<span className="font-bold" >Single Room:</span> US$245 - Professional / US$225 - Student ( US$70 per night)
 						</li>
 						</ul>
 						</li>
 					</ul>
 					<div className="flex flex-wrap justify-center my-2">
-						< p className="ml-4 text-base font-normal text-gray-700 dark:text-gray-400 mr-3 mt-2">For any inquiries, feel free to contact us at: tsyp@ieee.org</p>
+						< p className="ml-4 text-base font-normal text-gray-700 dark:text-gray-300 mr-3 mt-2">For any inquiries, feel free to contact us at: tsyp@ieee.org</p>
 						<Link
 							to={"https://forms.gle/WJwqbzMPE8PGcTmv8"}
 							target="_blank"
--- src/app/pages/TNChallenge.page.jsx
+++ src/app/pages/TNChallenge.page.jsx
@@ -1,110 +1,271 @@
+import { useEffect, useState } from "react";
 import CustomImage from "../components/CustomImage";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 export function TNChallengePage() {
-  const [tasksData, setTasksData] = useState([]);
-  const [taskSelected, settaskSelected] = useState(null);
-  const [currentSlide, setCurrentSlide] = useState(0);
+	const [tasksData, setTasksData] = useState([]);
+	const [taskSelected, settaskSelected] = useState(null);
+	const [currentSlide, setCurrentSlide] = useState(0);
 
-  useEffect(() => {
-    const getData = async () => {
-      fetch("/assets/tn-tasks.json")
-        .then((response) => response.json())
-        .then((response) => {
-          setTasksData(response);
-          settaskSelected(0);
-        })
-        .catch((err) => console.error(err));
-    };
-    getData();
-  }, []);
+	useEffect(() => {
+		const getData = async () => {
+			fetch("/assets/tn-tasks.json")
+				.then((response) => response.json())
+				.then((response) => {
+					setTasksData(response);
+					settaskSelected(0);
+				})
+				.catch((err) => console.error(err));
+		};
+		getData();
+	}, []);
 
-  const handleNextSlide = () => {
-    if (tasksData[taskSelected].sessions) {
-      setCurrentSlide((prev) => (prev + 1) % tasksData[taskSelected].sessions.length);
-    }
-  };
+	const handleNextSlide = () => {
+		if (tasksData[taskSelected].sessions) {
+			setCurrentSlide(
+				(prev) => (prev + 1) % tasksData[taskSelected].sessions.length
+			);
+		}
+	};
 
-  const handlePrevSlide = () => {
-    if (tasksData[taskSelected].sessions) {
-      setCurrentSlide((prev) => (prev - 1 + tasksData[taskSelected].sessions.length) % tasksData[taskSelected].sessions.length);
-    }
-  };
+	const handlePrevSlide = () => {
+		if (tasksData[taskSelected].sessions) {
+			setCurrentSlide(
+				(prev) =>
+					(prev - 1 + tasksData[taskSelected].sessions.length) %
+					tasksData[taskSelected].sessions.length
+			);
+		}
+	};
 
-  return (
-    <div className="h-full">
-      <div className="col-span-12 lg:py-20 md:mx-auto">
-        <h1 className="text-center text-4xl font-bold tracking-tight lg:text-center lg:text-6xl lg:font-extrabold lg:leading-none">
-          Tunisia 2056 Challenge
-        </h1>
-      </div>
+	return (
+		<div className="h-full">
+			<div className="col-span-12 md:mx-auto lg:py-20">
+				<h1 className=" text-center text-4xl font-bold tracking-tight  lg:text-center lg:text-6xl lg:font-extrabold lg:leading-none">
+					Tunisia 2056 Challenge
+				</h1>
+			</div>
 
-     
+			<section className="mb-12 grid items-center justify-center gap-4 py-8 md:grid-cols-12">
+				<div className="col-span-6 mx-auto mb-4 md:order-last">
+					<CustomImage
+						src={"https://i.imgur.com/jaV94we.png"}
+						className=" mx-auto h-52  rounded-2xl sm:h-96"
+						alt="tn2056"
+					/>
+				</div>
+				<div className="col-span-6">
+					<h1 className="mb-4 text-lg font-medium  lg:mb-7 lg:text-start lg:text-xl lg:font-normal">
+						The Tunisia 2056 Challenge (TN2056 Challenge) embodies the
+						transformative vision for TSYP in its 12th edition, emphasizing
+						collaboration among Tunisia Student Branches to tackle critical
+						challenges facing Tunisia and envision its future by 2056 through
+						the launch of innovative start-ups.
+					</h1>
+					<div></div>
+				</div>
+			</section>
 
-      <div className="col-span-12 pb-16 md:mx-auto">
-        <h1 className="text-center text-4xl font-bold tracking-tight lg:text-center lg:text-6xl lg:font-extrabold lg:leading-none">
-          TN 2056 Tasks
-        </h1>
-      </div>
-      
-      <div className="flex flex-row gap-4 w-4/5 mx-auto">
-        <div className="basis-1/3 hidden md:block">
-          {tasksData.map((task, index) => (
-            <div
-              key={index}
-              className="cursor-pointer border rounded-lg dark:bg-neutral-900 dark:border-neutral-900 my-4 hover:bg-slate-100"
-              onClick={() => {
-                settaskSelected(index);
-                setCurrentSlide(0); // Reset slide index when selecting a new task
-              }}
-            >
-              <div className="text-xs text-center text-gray-800 transition duration-700 dark:text-white py-4">
-                <span>{task.name}</span>
-              </div>
-            </div>
-          ))}
-        </div>
-        
-        <div className="basis-3/3 md:basis-2/3 py-4">
-          <div className="hidden md:block">
-            {taskSelected != null && (
-              tasksData[taskSelected].sessions ? (
-                <div className="relative">
-                  <img
-                    src={tasksData[taskSelected].sessions[currentSlide].url}
-                    alt={tasksData[taskSelected].sessions[currentSlide].sessionName}
-                    className="border rounded-lg h-3-5"
-                  />
-                  <button onClick={handlePrevSlide} className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded">
-				  &lt;
-                  </button>
-                  <button onClick={handleNextSlide} className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-gray-800 text-white p-2 rounded">
-                   &gt;
-                  </button>
-                </div>
-              ) : (
-                <img src={tasksData[taskSelected].url} alt={tasksData[taskSelected].name} className="border rounded-lg h-3-5" />
-              )
-            )}
-          </div>
+			<div className="col-span-12 pb-16 md:mx-auto">
+				<h1 className=" text-center text-4xl font-bold tracking-tight  lg:text-center lg:text-6xl lg:font-extrabold lg:leading-none">
+					About Info Session
+				</h1>
+			</div>
+			<section className="mb-12 grid items-center justify-center gap-4 py-8 md:grid-cols-12 ">
+				<div className="  col-span-6 mx-auto  mb-4 w-full text-center ">
+					<iframe
+						className="mx-auto h-56 w-full rounded-2xl shadow-lg sm:h-96"
+						src="https://www.youtube.com/embed/lCOTmTw0DWA?si=Xdw5N58MXQ7Y30JT"
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						referrerpolicy="strict-origin-when-cross-origin"
+						allowfullscreen
+					></iframe>
+				</div>
+				<div className="col-span-6 lg:ml-12">
+					<h1 className="mb-4  text-lg font-medium   lg:mb-7 lg:text-start lg:text-xl lg:font-normal">
+						Our first step in the TN2056 Challenge was the info sessions phase.
+						We began with a chair meeting where we introduced the 12th edition
+						of TSYP and explained the concept of this new challenge. We then
+						asked the chairs to hold info sessions for their student branches,
+						as they are now our representatives. Following this, we held three
+						Webex sessions, each focusing on one of our three sectors: Health
+						Tech, Agritech, and Creative & Cultural Industries. These sessions
+						were assisted by outstanding experts from these domains, providing
+						valuable insights and guidance.
+					</h1>
+					<div>
+						<Link
+							// type="button"
+							to="https://drive.google.com/drive/folders/1k-84a_iVUq_8K7KhfXSJJJubsGRTuuJt"
+							target="_blank"
+							rel="noreferrer"
+							className="relative inline min-w-[120px] items-center justify-center rounded-full border-2 border-gray-600 px-6 py-2 text-center text-sm font-medium transition hover:border-gray-900 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-gray-400 dark:bg-black  dark:hover:bg-white dark:hover:bg-opacity-20 dark:focus:ring-blue-800"
+						>
+							{/* <div className="flex flex-wrap items-center justify-start gap-2"> */}
+							<span className="">Learn more about TN 2056 Challenge</span>
+						</Link>
+					</div>
+				</div>
+			</section>
+			<div className="col-span-12 pb-16 md:mx-auto">
+				<h1 className=" text-center text-4xl font-bold tracking-tight  lg:text-center lg:text-6xl lg:font-extrabold lg:leading-none">
+					Meet Our Experts
+				</h1>
+			</div>
+			<section className="py-8 md:py-10">
+				<div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+					<div className="grid gap-4">
+						<iframe
+							className="mx-auto h-56 w-full rounded-2xl shadow-lg sm:h-96"
+							src="https://www.youtube.com/embed/fGtaovfEMsk?si=qeKhTqVvShbma6Hi"
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							referrerpolicy="strict-origin-when-cross-origin"
+							allowfullscreen
+						></iframe>
+					</div>
+					<div className="grid gap-4">
+						<iframe
+							className="mx-auto h-56 w-full rounded-2xl shadow-lg sm:h-96"
+							src="https://www.youtube.com/embed/yQifdi5ayzk?si=U_JuQwftTdTBmR4O"
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							referrerpolicy="strict-origin-when-cross-origin"
+							allowfullscreen
+						></iframe>
+					</div>
+					<div className="grid gap-4">
+						<iframe
+							className="mx-auto h-56 w-full rounded-2xl shadow-lg sm:h-96"
+							src="https://www.youtube.com/embed/jYsry2bVTto?si=fwG37715PnjpIL7t"
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							referrerpolicy="strict-origin-when-cross-origin"
+							allowfullscreen
+						></iframe>
+					</div>
+				</div>
+			</section>
+			<div className="col-span-12 py-16 md:mx-auto">
+				<h1 className="text-center text-4xl font-bold tracking-tight lg:text-center lg:text-6xl lg:font-extrabold lg:leading-none">
+					TN 2056 Tasks
+				</h1>
+			</div>
 
-          
-          <div className="block md:hidden">
-            {tasksData.map((task, index) =>
-              task.sessions ? (
-                <div key={index}>
-                  {task.sessions.map((session, sessionIndex) => (
-                    <img key={sessionIndex} src={session.url} alt={session.sessionName} className="border rounded-lg h-3-5 my-2" />
-                  ))}
-                </div>
-              ) : (
-                <img key={index} src={task.url} alt={task.name} className="border rounded-lg h-3-5 my-2" />
-              )
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="mx-auto flex w-4/5 flex-row gap-4">
+				<div className="hidden basis-1/3 md:block">
+					{tasksData.map((task, index) => (
+						<div
+							key={index}
+							className="my-4 cursor-pointer rounded-lg border hover:bg-slate-100 dark:border-neutral-900 dark:bg-neutral-900"
+							onClick={() => {
+								settaskSelected(index);
+								setCurrentSlide(0); // Reset slide index when selecting a new task
+							}}
+						>
+							<div className="py-4 text-center text-xs text-gray-800 transition duration-700 dark:text-white">
+								<span>{task.name}</span>
+							</div>
+						</div>
+					))}
+				</div>
+
+				<div className="basis-3/3 py-4 md:basis-2/3">
+					<div className="hidden md:block">
+						{taskSelected != null &&
+							(tasksData[taskSelected].sessions ? (
+								<div className="relative">
+									<img
+										src={tasksData[taskSelected].sessions[currentSlide].url}
+										alt={
+											tasksData[taskSelected].sessions[currentSlide].sessionName
+										}
+										className="h-3-5 rounded-lg border"
+									/>
+									<button
+                    onClick={handlePrevSlide}
+										type="button"
+										class="absolute left-2 top-1/2 me-2 rotate-180 inline-flex items-center rounded-lg bg-stone-400 dark:bg-gray-800 p-2.5 text-center text-sm font-medium text-white"
+									>
+										<svg
+											class="h-5 w-5"
+											aria-hidden="true"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 14 10"
+										>
+											<path
+												stroke="currentColor"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M1 5h12m0 0L9 1m4 4L9 9"
+											/>
+										</svg>					
+									</button>
+                  <button
+                    onClick={handleNextSlide}
+										type="button"
+										class="absolute right-2 top-1/2  me-2 inline-flex items-center rounded-lg bg-stone-400 dark:bg-gray-800 p-2.5 text-center text-sm font-medium text-white "
+									>
+										<svg
+											class="h-5 w-5"
+											aria-hidden="true"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 14 10"
+										>
+											<path
+												stroke="currentColor"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M1 5h12m0 0L9 1m4 4L9 9"
+											/>
+										</svg>
+                    
+										<span class="sr-only">Icon description</span>
+									</button>
+								</div>
+							) : (
+								<img
+									src={tasksData[taskSelected].url}
+									alt={tasksData[taskSelected].name}
+									className="h-3-5 rounded-lg border"
+								/>
+							))}
+					</div>
+
+					<div className="block md:hidden">
+						{tasksData.map((task, index) =>
+							task.sessions ? (
+								<div key={index}>
+									{task.sessions.map((session, sessionIndex) => (
+										<img
+											key={sessionIndex}
+											src={session.url}
+											alt={session.sessionName}
+											className="h-3-5 my-2 rounded-lg border"
+										/>
+									))}
+								</div>
+							) : (
+								<img
+									key={index}
+									src={task.url}
+									alt={task.name}
+									className="h-3-5 my-2 rounded-lg border"
+								/>
+							)
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
